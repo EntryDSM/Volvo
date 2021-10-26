c2b60027455e418603e6ca33ebeb5a31ab21cf3e
kimulchan <leerabi577@gmail.com>
2021-10-27 00:10:44 +0900

    ⚡ :: (#1) 비구조화 할당 방식 바꿈

--- volvo/src/components/grade/column/VolunteerColumn.tsx
+++ volvo/src/components/grade/column/VolunteerColumn.tsx
@@ -1,20 +1,21 @@
-import React from "react";
-import * as s from "../style";
-interface VolunteerColumnProps{
-    text?:string,
-    unit:string
+import React from 'react';
+import * as s from '../style';
+interface VolunteerColumnProps {
+  text?: string;
+  unit: string;
 }
 
-const VolunteerColumn:React.FC<VolunteerColumnProps>=({text,unit})=>{
-    return(
+const VolunteerColumn: React.FC<VolunteerColumnProps> = props => {
+  const { text, unit } = props;
+  return (
     <s.VolunteerColumnWrapper>
-        {text&&<s.VolunteerColumnTitle>{text}</s.VolunteerColumnTitle>}
-        <s.VolunteerColumnInputWrapper>
-            <input type="number" defaultValue={0}/>
-            <p>{unit}</p>
-        </s.VolunteerColumnInputWrapper>
+      {text && <s.VolunteerColumnTitle>{text}</s.VolunteerColumnTitle>}
+      <s.VolunteerColumnInputWrapper>
+        <input type='number' defaultValue={0} />
+        <p>{unit}</p>
+      </s.VolunteerColumnInputWrapper>
     </s.VolunteerColumnWrapper>
-    )
-}
+  );
+};
 
 export default VolunteerColumn;
