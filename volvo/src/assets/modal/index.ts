04cdf5d44b7163c4195d8bafbc53086051245cdc
hyejun <hyejun0608@naver.com>
2021-10-27 01:39:01 +0900

    ⚡ :: (#18) 기존 이미지 가져오기

---
+++ volvo/src/assets/modal/index.ts
@@ -0,0 +1,8 @@
+export { default as move } from './move.svg';
+export { default as move_active } from './move_active.svg';
+export { default as password_reset_success } from './password_reset_success.svg';
+export { default as submit } from './submit.svg';
+export { default as file_error } from './file_error.svg';
+export { default as access_error } from './access_error.svg';
+export { default as XBtn } from './xImg.svg';
+export { default as search } from './searchImg.svg';
