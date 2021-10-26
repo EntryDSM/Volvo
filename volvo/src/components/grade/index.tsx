440d1d9a7e0e61d27a4e125ff8c88818fe80d006
kimulchan <leerabi577@gmail.com>
2021-10-27 00:14:30 +0900

    ⚡ :: (#2) 페이지에 Grade 추가

--- volvo/src/components/grade/index.tsx
+++ volvo/src/components/grade/index.tsx
@@ -1,4 +1,5 @@
 import * as s from "./style";
+import GradeTable from "./table/GradeTable";
 import Volunteer from "./table/Volunteer";
 import { currentYear } from '../../constance/default';
 
@@ -9,7 +10,13 @@ function Grade (){
                 <s.GradeSchoolTitle>대덕소프트웨어마이스터고등학교</s.GradeSchoolTitle>
                 <s.GradeBigTitle>{currentYear}년 지원자 성적 입력</s.GradeBigTitle>
             </s.GradeTitles>
-            <Volunteer/>
+            <Volunteer></Volunteer>
+            <GradeTable></GradeTable>
+            <s.PageMoveWrapper>
+                <button>이전</button>
+                <button>다음</button>
+            </s.PageMoveWrapper>
+            <div>footer</div>
         </s.GradeWrapper>
     )
 }
