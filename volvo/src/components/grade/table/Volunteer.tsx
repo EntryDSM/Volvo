d9a7ff527372a42436ec37c0faa23231520120aa
kimulchan <leerabi577@gmail.com>
2021-10-26 22:21:17 +0900

    ⚡ :: (#1) VolunteerTable 크기 정형화

--- volvo/src/components/grade/table/Volunteer.tsx
+++ volvo/src/components/grade/table/Volunteer.tsx
@@ -1,22 +1,25 @@
+import VolunteerColumn from "../column/VolunteerColumn";
 import * as s from "../style";
 
+
 function Volunteer(){
+    
+
     return(
         <s.VolunteerWrapper>
             <p>봉사 {'&'} 출석</p>
-            <s.VolunteerTable>
+            <s.VolunteerTable style={{width:"100%",tableLayout:"fixed"}}>
                 <tr>
                     <th>봉사시간</th>
-                    <td></td>
+                    <td><VolunteerColumn text={""} unit={"시간"}></VolunteerColumn></td>
                     <td></td>
                 </tr>
                 <tr>
                     <th rowSpan={2}>출석정보</th>
-                    <td></td>
+                    <td><VolunteerColumn text={"전체 미인정 결석 일수"} unit={"일"}></VolunteerColumn></td>
                     <td></td>
                 </tr>
                 <tr>
-                    
                     <td></td>
                     <td></td>
                 </tr>
