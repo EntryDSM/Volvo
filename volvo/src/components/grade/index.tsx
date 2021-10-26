88d7ff3ca278d068265f9c8cf9d3e3b500b80418
kimulchan <leerabi577@gmail.com>
2021-10-27 00:07:05 +0900

    ⚡:: (#1) current year 추가

--- volvo/src/components/grade/index.tsx
+++ volvo/src/components/grade/index.tsx
@@ -1,12 +1,13 @@
 import * as s from "./style";
 import Volunteer from "./table/Volunteer";
+import { currentYear } from '../../constance/default';
 
 function Grade (){
     return (
         <s.GradeWrapper>
             <s.GradeTitles>
                 <s.GradeSchoolTitle>대덕소프트웨어마이스터고등학교</s.GradeSchoolTitle>
-                <s.GradeBigTitle>2022년 지원자 성적 입력</s.GradeBigTitle>
+                <s.GradeBigTitle>{currentYear}년 지원자 성적 입력</s.GradeBigTitle>
             </s.GradeTitles>
             <Volunteer/>
         </s.GradeWrapper>
