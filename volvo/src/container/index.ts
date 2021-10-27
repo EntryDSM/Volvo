860f9e6b4100a50c0d0e5cd313dd19016778dd9b
hyejun <hyejun0608@naver.com>
2021-10-27 11:02:44 +0900

    ⚡:: (#15) 기존 container 코드 가져오기

--- volvo/src/container/index.ts
+++ volvo/src/container/index.ts
@@ -1,1 +1,5 @@
-export { default as HeaderContainer } from './header';
+export { default as MainContainer } from './main';
+export { default as SignUpContainer } from './signup';
+export { default as ModalContainer } from './modal';
+export { default as SelectTypeContainer } from './selectType';
+export { default as PreviewContainer } from './preview';
