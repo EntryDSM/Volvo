ddab813ccc1ea1d041ecdf7e1e8aadd6fecf4e87
hyejun <hyejun0608@naver.com>
2021-10-27 11:10:04 +0900

    ⚡:: (#16) root reducer, root middleware

--- volvo/src/modules/redux/reducer/index.ts
+++ volvo/src/modules/redux/reducer/index.ts
@@ -1,6 +1,25 @@
 import { combineReducers } from 'redux';
+import userReducer from './user';
+import modalReducer from './modal';
+import statusReducer from './status';
+import signupReducer from './signup';
+import signinReducer from './signin';
+import resetPasswordReducer from './resetPassword';
+import authReducer from './auth';
+import selectTypeReducer from './selectType';
+import previewReducer from './preview';
 
-const rootReducer = combineReducers({});
+const rootReducer = combineReducers({
+  user: userReducer,
+  modal: modalReducer,
+  status: statusReducer,
+  signup: signupReducer,
+  signin: signinReducer,
+  resetPassword: resetPasswordReducer,
+  auth: authReducer,
+  selectType: selectTypeReducer,
+  preview: previewReducer,
+});
 
 export type reducerType = ReturnType<typeof rootReducer>;
 export default rootReducer;
