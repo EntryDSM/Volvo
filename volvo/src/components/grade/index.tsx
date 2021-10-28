ceb997e51933503314c5c26842bf3b8d5f1c1e20
kimulchan <leerabi577@gmail.com>
2021-10-28 23:33:22 +0900

    ⚡ :: (#2) 여닫 테그 수정

--- volvo/src/components/grade/index.tsx
+++ volvo/src/components/grade/index.tsx
@@ -2,16 +2,45 @@ import * as s from "./style";
 import GradeTable from "./table/GradeTable";
 import Volunteer from "./table/Volunteer";
 import { currentYear } from '../../constance/default';
+import { GradeType } from "../../constance/grade";
+ interface Props {
+    volunteerTime: number,
+    absence: number,//결석
+    leave: number,//조퇴
+    lateness: number,//지각
+    truancy: number,//무단 결석
+    grade: GradeType,
+    isSuccessSaveGrade?: boolean,
+    setInput:(paylode:{name:string,value:number})=>void
+    setGrade:(paylode:{subject:string,value:string,stateSequence:number})=>void
+}
 
-function Grade (){
+const Grade:React.FC<Props> =({
+    volunteerTime,
+    absence,
+    leave,
+    lateness,
+    truancy,
+    grade,
+    isSuccessSaveGrade,
+    setInput,
+    setGrade
+})=>{
+    
     return (
         <s.GradeWrapper>
             <s.GradeTitles>
                 <s.GradeSchoolTitle>대덕소프트웨어마이스터고등학교</s.GradeSchoolTitle>
                 <s.GradeBigTitle>{currentYear}년 지원자 성적 입력</s.GradeBigTitle>
             </s.GradeTitles>
-            <Volunteer></Volunteer>
-            <GradeTable></GradeTable>
+            <Volunteer 
+                volunteerTime={volunteerTime}
+                absence={absence}
+                leave={leave}
+                lateness={lateness}
+                truancy={truancy}
+                setInput={setInput} />
+            <GradeTable {...grade} />
             <s.PageMoveWrapper>
                 <button>이전</button>
                 <button>다음</button>
