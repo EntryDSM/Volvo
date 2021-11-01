eed091043e64722542d19c48199f8aab952e4a12
kimulchan <leerabi577@gmail.com>
2021-11-02 00:42:31 +0900

    ⚡ :: GradeColumn type 및 param 변경

--- volvo/src/components/grade/column/GradeColumn.tsx
+++ volvo/src/components/grade/column/GradeColumn.tsx
@@ -1,28 +1,44 @@
-import * as s from "../style";
-import React,{useState} from "react";
-import {GColumnFactor} from "../../../constance/grade";
-interface Props{
-    gradeState:string;
-    stateSequence:number;
+import * as s from '../style';
+import React, { useState } from 'react';
+import { GColumnFactor } from '../../../constance/grade';
+import { Subject, SubjectScore } from '../../../types';
+interface Props {
+  gradeState: string;
+  stateSequence: number;
+  setGrade: (paylode: { subject: Subject; value: SubjectScore; stateSequence: number }) => void;
+  subjectEn: Subject;
 }
 
-const GradeColumn:React.FC<Props>=(props)=>{
-    const {gradeState,stateSequence}= props
-    const [isOpen, setIsOpen]=useState<boolean>(false);
-    const GColumnOnclickEvent =(props:string)=>{
-        
-    } 
-    return (
-        <s.GradeColumnFontWrapper>
-            {isOpen?
-                GColumnFactor.map(props=>(<p onClick={(e)=>{
-                    setIsOpen(!isOpen);
-                    GColumnOnclickEvent(props);
-                }}>{props}</p>)):
-                <p onClick={()=>setIsOpen(!isOpen)}>{gradeState}</p>
-            }
-        </s.GradeColumnFontWrapper>
-    );
-}
+const GradeColumn: React.FC<Props> = props => {
+  const { gradeState, stateSequence, subjectEn, setGrade } = props;
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const GColumnOnclickEvent = (props: SubjectScore) => {
+    const setStateObject = {
+      subject: subjectEn,
+      value: props,
+      stateSequence: stateSequence,
+    };
+    setGrade(setStateObject);
+  };
+  return (
+    <s.GradeColumnFontWrapper>
+      {isOpen ? (
+        GColumnFactor.map((props, i) => (
+          <p
+            key={i}
+            onClick={() => {
+              setIsOpen(!isOpen);
+              GColumnOnclickEvent(props);
+            }}
+          >
+            {props}
+          </p>
+        ))
+      ) : (
+        <p onClick={() => setIsOpen(!isOpen)}>{gradeState}</p>
+      )}
+    </s.GradeColumnFontWrapper>
+  );
+};
 
 export default GradeColumn;
