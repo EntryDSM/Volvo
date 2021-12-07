58e5057cea565e1ecb181180e7c5ab64ac8a624c
kimulchan <leerabi577@gmail.com>
2021-12-08 00:45:30 +0900

    ⚡ :: get grade state type 추가

--- volvo/src/types.ts
+++ volvo/src/types.ts
@@ -22,6 +22,20 @@ export interface GradeState {
   isSuccessSaveGrade?: boolean,
 }
 
+export interface GetGradeState {
+  volunteer_time:number,
+  day_absence_count:number,
+  lecture_absence_count:number,
+  lateness_count:number,
+  early_leave_count:number,
+  korean_grade:string,
+  social_grade:string,
+  history_grade:string,
+  math_grade:string,
+  science_grade:string,
+  english_grade:string,
+  tech_and_home_grade:string
+}
 // select type
 export interface selectTypeStateInterface {
   educationalStatus: string | null;
