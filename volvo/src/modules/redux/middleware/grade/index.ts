6b668283084755859f0214783f47452bfcdf461e
kimulchan <leerabi577@gmail.com>
2021-12-08 00:43:50 +0900

    ⚡ :: grade get success 시에 상태 변수 추가

--- volvo/src/modules/redux/middleware/grade/index.ts
+++ volvo/src/modules/redux/middleware/grade/index.ts
@@ -1,16 +1,34 @@
 import {  AxiosResponse } from 'axios';
-import { call, debounce, put, takeLatest } from 'redux-saga/effects';
-import { GradeState } from '../../../../types';
+import { call, debounce, put, takeLatest,select } from 'redux-saga/effects';
+import { GetGradeState, GradeState } from '../../../../types';
 import { getGradeApi, setGrade } from '../../../../util/api/grade';
 import { getGrade, saveGrade } from '../../action/grade';
 import {  GET_GRADE, SAVE_GRADE, SET_GRADE, SET_INPUT } from '../../action/grade/interface';
+import { reducerType } from '../../reducer';
 
 const SET_STATE_ACTION = [SET_GRADE, SET_INPUT];
 
 export function* gradeGetSaga() {
   try {
-    const response: AxiosResponse<GradeState> = yield call(getGradeApi);
-    yield put(getGrade.success(response.data));
+    const response: AxiosResponse<GetGradeState> = yield call(getGradeApi);
+    const rData = response.data;
+    console.log(rData);
+    yield put(getGrade.success({
+      absence:rData.day_absence_count,
+      grade:{
+        english:rData.english_grade,
+        history:rData.history_grade,
+        korean:rData.korean_grade,
+        math:rData.math_grade,
+        science:rData.science_grade,
+        social:rData.social_grade,
+        technical:rData.tech_and_home_grade
+      },
+      lateness:rData.lateness_count,
+      leave:rData.early_leave_count,
+      truancy:rData.lecture_absence_count,
+      volunteerTime:rData.volunteer_time
+    }));
   } catch (error:any) {
     if (error.response?.data) {
       yield put(getGrade.failure());
@@ -20,9 +38,10 @@ export function* gradeGetSaga() {
   }
 }
 
-export function* gradeSaveSaga(action: ReturnType<typeof saveGrade.request>) {
+export function* gradeSaveSaga() {
   try {
-    yield call(setGrade, action.payload);
+    const state:reducerType['grade'] = yield select((state:reducerType):reducerType["grade"]=>state.grade)
+    yield call(setGrade,state );
     yield put(saveGrade.success());
   } catch (error:any) {
     if (error.response?.data) {
