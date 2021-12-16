8d152f3c0a57ebffb528b0ec95c10b6282be1b84
강석현 <tjrgus0713@gmail.com>
2021-12-16 16:06:12 +0900

    ⚡️ :: (#66) 자동저장 기능 미들웨어 생성

---
+++ volvo/src/modules/redux/middleware/introduction/index.ts
@@ -0,0 +1,145 @@
+import { call, debounce, put, select, takeLatest } from '@redux-saga/core/effects';
+import { responseGenerator } from '../../../../types';
+import { getIntro, saveIntro, saveStudy, getStudy } from '../../../../util/api/introduction';
+import createRequestSaga from '../../../../util/saga/createRequestSaga';
+import {
+  CHANGE_SELFINTRODUCE,
+  CHANGE_STUDYPLAN,
+  GET_SELFINTRO,
+  GET_STUDYPLAN,
+  SAVE_INTRO,
+  SAVE_STUDYPLAN,
+} from '../../action/introduction/interface';
+import { reducerType } from '../../reducer';
+import IntroductionState from '../../reducer/introduction/interface';
+
+// export const introSaveSaga = createRequestSaga(SAVE_INTRO, saveIntro);
+const getStateFunc = (state: reducerType): reducerType['introduction'] => state.introduction;
+
+const introSaveSaga = function* () {
+  const action = 'introduction/SAVE_INTRO';
+  const SUCCESS = `${action}_SUCCESS`;
+  const FAILURE = `${action}_FAILURE`;
+  const state: IntroductionState = yield select(getStateFunc);
+  const accessToken = localStorage.getItem('access_token') || '';
+  try {
+    const response: responseGenerator = yield call(saveIntro, accessToken, state.selfIntroduce);
+    yield put({
+      type: SUCCESS,
+      payload: response ? response.data : null,
+    });
+  } catch (error: any) {
+    if (error.response?.data) {
+      yield put({
+        type: FAILURE,
+        payload: { ...error.response.data, type: action },
+      });
+    } else {
+      yield put({
+        type: FAILURE,
+        payload: {
+          message: 'Network Error',
+          status: 500,
+        },
+      });
+    }
+  }
+};
+const studyPlanSaveSaga = function* () {
+  const action = 'introduction/SAVE_STUDYPLAN';
+  const SUCCESS = `${action}_SUCCESS`;
+  const FAILURE = `${action}_FAILURE`;
+  const state: IntroductionState = yield select(getStateFunc);
+  const accessToken = localStorage.getItem('access_token') || '';
+  try {
+    const response: responseGenerator = yield call(saveStudy, accessToken, state.studyPlan);
+    yield put({
+      type: SUCCESS,
+      payload: response ? response.data : null,
+    });
+  } catch (error: any) {
+    if (error.response?.data) {
+      yield put({
+        type: FAILURE,
+        payload: { ...error.response.data, type: action },
+      });
+    } else {
+      yield put({
+        type: FAILURE,
+        payload: {
+          message: 'Network Error',
+          status: 500,
+        },
+      });
+    }
+  }
+};
+
+const getSelfIntroSaga = function* () {
+  const action = 'introduction/GET_SELFINTRO';
+  const SUCCESS = `${action}_SUCCESS`;
+  const FAILURE = `${action}_FAILURE`;
+  const state: IntroductionState = yield select(getStateFunc);
+  const accessToken = localStorage.getItem('access_token') || '';
+  try {
+    const response: responseGenerator = yield call(getIntro, accessToken);
+    yield put({
+      type: SUCCESS,
+      payload: response ? response.data.content : null,
+    });
+  } catch (error: any) {
+    if (error.response?.data) {
+      yield put({
+        type: FAILURE,
+        payload: { ...error.response.data, type: action },
+      });
+    } else {
+      yield put({
+        type: FAILURE,
+        payload: {
+          message: 'Network Error',
+          status: 500,
+        },
+      });
+    }
+  }
+};
+const getStudySaga = function* () {
+  const action = 'introduction/GET_STUDYPLAN';
+  const SUCCESS = `${action}_SUCCESS`;
+  const FAILURE = `${action}_FAILURE`;
+  const state: IntroductionState = yield select(getStateFunc);
+  const accessToken = localStorage.getItem('access_token') || '';
+  try {
+    const response: responseGenerator = yield call(getStudy, accessToken);
+    yield put({
+      type: SUCCESS,
+      payload: response ? response.data.content : null,
+    });
+  } catch (error: any) {
+    if (error.response?.data) {
+      yield put({
+        type: FAILURE,
+        payload: { ...error.response.data, type: action },
+      });
+    } else {
+      yield put({
+        type: FAILURE,
+        payload: {
+          message: 'Network Error',
+          status: 500,
+        },
+      });
+    }
+  }
+};
+function* introductionSaga() {
+  yield takeLatest(SAVE_INTRO, introSaveSaga);
+  yield debounce(3000, CHANGE_SELFINTRODUCE, introSaveSaga);
+  yield takeLatest(SAVE_STUDYPLAN, studyPlanSaveSaga);
+  yield debounce(3000, CHANGE_STUDYPLAN, studyPlanSaveSaga);
+  yield takeLatest(GET_SELFINTRO, getSelfIntroSaga);
+  yield takeLatest(GET_STUDYPLAN, getStudySaga);
+}
+
+export default introductionSaga;
