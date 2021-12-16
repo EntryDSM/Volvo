d28acab5d1d2ff01a0d8a87105fe27afcaf30c7c
강석현 <tjrgus0713@gmail.com>
2021-12-16 16:06:41 +0900

    ⚡️ :: (#66) 자동저장 기능 리듀서 연결

---
+++ volvo/src/modules/redux/reducer/introduction/interface/index.ts
@@ -0,0 +1,9 @@
+import { error } from '../../../../../types';
+
+interface IntroductionState {
+  selfIntroduce: string;
+  studyPlan: string;
+  error: error;
+}
+
+export default IntroductionState;
