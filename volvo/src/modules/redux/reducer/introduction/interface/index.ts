2c83b56096f2ea50a5cea080bb696fd9b9048890
hyejun <hyejun0608@naver.com>
2021-12-17 21:53:44 +0900

    ⚡️:: (#70) isSuccessSetIntro & isSuccessSetStudy 상태 추가

--- volvo/src/modules/redux/reducer/introduction/interface/index.ts
+++ volvo/src/modules/redux/reducer/introduction/interface/index.ts
@@ -4,6 +4,8 @@ interface IntroductionState {
   selfIntroduce: string;
   studyPlan: string;
   error: error;
+  isSuccessSetIntro: boolean | undefined;
+  isSuccessSetStudy: boolean | undefined;
 }
 
 export default IntroductionState;
