e2f961f638ccf971fe7b98b09b830a45458333b3
hyejun <hyejun0608@naver.com>
2021-12-18 15:20:35 +0900

    ⚡️:: (#84) isFinalSubmit & isLogin func

--- volvo/src/util/api/default/index.ts
+++ volvo/src/util/api/default/index.ts
@@ -22,3 +22,19 @@ export const getRequestWithAccessToken = (
   });
   return request;
 };
+
+export const isFinalSubmit = (isFinalSubmitDone: boolean): boolean => {
+  if (isFinalSubmitDone) {
+    alert(
+      '최종제출되었습니다. 접근하시려면 최종제출을 취소하고 접근해주세요. 최종제출은 학교에 연락하여 취소하셔야 합니다.',
+    );
+    return true;
+  } else return false;
+};
+
+export const isLogin = (accessToken: string | null): boolean => {
+  if (!accessToken) {
+    alert('로그인 후 이용하실 수 있습니다.');
+    return true;
+  } else return false;
+};
