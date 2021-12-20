448a9c8ce19da902a755a03682e156b4891e5637
hyejun <hyejun0608@naver.com>
2021-12-20 22:22:51 +0900

    ⚡️:: (#88) saga 코드를 정리

--- volvo/src/modules/redux/middleware/information/index.ts
+++ volvo/src/modules/redux/middleware/information/index.ts
@@ -40,18 +40,17 @@ const getStateFunc = (state: reducerType): reducerType['information'] => state.i
 const getSelectTypeStateFunc = (state: reducerType): reducerType['selectType'] => state.selectType;
 const getUserName = (state: reducerType) => state.user.name;
 
-export const informationGetSaga = createRequestSaga(GET_INFORMATION, getInformation);
-export const graduateInformationGetSaga = createRequestSaga(
+const informationGetSaga = createRequestSaga(GET_INFORMATION, getInformation);
+const graduateInformationGetSaga = createRequestSaga(
   GET_GRADUATE_INFORMATION,
   getGraduateInformation,
 );
-export const userPhotoSaveSaga = createRequestSaga(USER_PICTURE, userPhoto);
-export const searchSchoolGetSaga = createRequestSaga(SEARCH_SCHOOL, getSearchSchool);
-export const gedScoreGetSaga = createRequestSaga(GET_GED_SCORE, getGedScore);
+const userPhotoSaveSaga = createRequestSaga(USER_PICTURE, userPhoto);
+const searchSchoolGetSaga = createRequestSaga(SEARCH_SCHOOL, getSearchSchool);
+const gedScoreGetSaga = createRequestSaga(GET_GED_SCORE, getGedScore);
 
 const informationSaveSaga = function* () {
-  const SUCCESS = 'INFORMATION/INFORMATION_SUCCESS';
-  const FAILURE = 'INFORMATION/INFORMATION_FAILURE';
+  const ACTION = 'INFORMATION/INFORMATION';
   const state: InformationState = yield select(getStateFunc);
   const userName: string = yield select(getUserName);
   const requestState: informationInterface = informationStateToRequest(state, userName);
@@ -59,54 +58,33 @@ const informationSaveSaga = function* () {
   try {
     const response: responseGenerator = yield call(information, accessToken, requestState);
     yield put({
-      type: SUCCESS,
+      type: `${ACTION}_SUCCESS`,
       payload: response ? response.data : null,
     });
   } catch (error: any) {
-    if (error.response?.data) {
-      yield put({
-        type: FAILURE,
-        payload: { ...error.response.data, type: 'INFORMATION/INFORMATION' },
-      });
-    } else {
-      yield put({
-        type: FAILURE,
-        payload: {
-          message: 'Network Error',
-          status: 500,
-        },
-      });
-    }
+    yield put({
+      type: `${ACTION}_FAILURE`,
+      payload: { ...error.response?.data, type: ACTION },
+    });
   }
 };
 
 const graduateInformationSaveSaga = function* () {
-  const SUCCESS = 'INFORMATION/GRADUATE_INFORMATION_SUCCESS';
-  const FAILURE = 'INFORMATION/GRADUATE_INFORMATION_FAILURE';
+  const ACTION = 'INFORMATION/GRADUATE_INFORMATION';
   const state: InformationState = yield select(getStateFunc);
   const request = graduateInformationStateToRequest(state);
   const accessToken = localStorage.getItem('access_token') || '';
   try {
     const response: responseGenerator = yield call(graduateInformation, accessToken, request);
     yield put({
-      type: SUCCESS,
+      type: `${ACTION}_SUCCESS`,
       payload: response ? response.data : null,
     });
   } catch (error: any) {
-    if (error.response?.data) {
-      yield put({
-        type: FAILURE,
-        payload: { ...error.response.data, type: 'INFORMATION/GRADUATE_INFORMATION' },
-      });
-    } else {
-      yield put({
-        type: FAILURE,
-        payload: {
-          message: 'Network Error',
-          status: 500,
-        },
-      });
-    }
+    yield put({
+      type: `${ACTION}_FAILURE`,
+      payload: { ...error.response?.data, type: ACTION },
+    });
   }
 };
 
@@ -119,31 +97,20 @@ const proxySaga = function* () {
 };
 
 const gedScoreSaveSaga = function* () {
-  const SUCCESS = 'INFORMATION/GED_SCORE_SUCCESS';
-  const FAILURE = 'INFORMATION/GED_SCORE_FAILURE';
+  const ACTION = 'INFORMATION/GED_SCORE';
   const state: InformationState = yield select(getStateFunc);
   const accessToken = localStorage.getItem('access_token') || '';
   try {
     const response: responseGenerator = yield call(gedScore, accessToken, state.totalScore);
     yield put({
-      type: SUCCESS,
+      type: `${ACTION}_SUCCESS`,
       payload: response ? response.data : null,
     });
   } catch (error: any) {
-    if (error.response?.data) {
-      yield put({
-        type: FAILURE,
-        payload: { ...error.response.data, type: 'INFORMATION/GED_SCORE' },
-      });
-    } else {
-      yield put({
-        type: FAILURE,
-        payload: {
-          message: 'Network Error',
-          status: 500,
-        },
-      });
-    }
+    yield put({
+      type: `${ACTION}_FAILURE`,
+      payload: { ...error.response?.data, type: ACTION },
+    });
   }
 };
 
